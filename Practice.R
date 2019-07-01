f926e98fb39d5d1716470fa43354ee4ee07794b8
Plentir <yjy990810@naver.com>
2019-07-01 17:31:26 +0900

    연습

---
+++ Practice.R
@@ -0,0 +1,19 @@
+x1 <- 10  # 변수 지정하기
+x2 <- c(1, 5, 10, 15, 20)  # 여러 값을 하나의 변수에 지정하기
+x3 <- c(101:110)  # 수열을 하나의 변수에 지정하기
+x4 <- seq(101, 110)  # x3과 동일한 결과
+x5 <- seq(101, 110, by=3)  # 3씩 증가하는 수열을 하나의 변수에 지정하기
+y1 <- "hi"
+y2 <- c("a", "b", "hello")
+
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+print(x5)
+print(y1)
+print(y2)
+
+# 패키지 설치 및 로드하기
+# install.packages("package_name")
+# library("package_name")
