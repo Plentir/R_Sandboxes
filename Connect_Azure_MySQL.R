04cec10304a63de8d7bb61f12e10f0010182b595
Plentir <plentir@naver.com>
2020-08-17 02:40:15 +0900

    폴더로 정리

--- Connect_Azure_MySQL.R
+++
@@ -1,2 +0,0 @@
-library(odbc)
-con <- dbConnect(odbc::odbc(), .connection_string = "Driver={MySQL ODBC 8.0 Unicode Driver};SERVER={plen-tutoring.mysql.database.azure.com};DATABASE={web_crawling};USER={plentir@plen-tutoring};PASSWORD={yjy990810!};", timeout = 10)
